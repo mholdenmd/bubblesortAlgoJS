9d0f3ef51402252c96d8aab3d15c7580b7753875
mholdenmd <77797864+mholdenmd@users.noreply.github.com>
2021-07-19 12:40:38 -0400

    commit

---
+++ algos/test.js
@@ -0,0 +1,18 @@
+// let modal = document.querySelector('#results');
+// setTimeout(function() {
+//  modal.classList.remove('hidden');
+// }, 10000);
+// console.log('Results shown');
+
+// "use strict";
+// function logThis() {
+//  this.desc = "logger";
+//  console.log(this);
+// }
+// new logThis();
+
+const dessert = { type: 'pie' }; 
+dessert.type = 'pudding';
+
+const seconds = dessert;
+seconds.type = 'fruit';
